33fd502d4557f70cd7bf5f326d90e809c9606d36
“Yusuf-kehinde” <yusufkehinde1454@gmail.com>
2022-10-18 06:12:35 -0700

    Mandatory Tasks solved

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description: a fuction that prints the alphabet in lowercase
+ * followed by a new line
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
