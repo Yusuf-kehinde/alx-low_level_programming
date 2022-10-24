289d89731aaf4e3f4a925ef5e9723abdaf6d74eb
“Yusuf-kehinde” <yusufkehinde1454@gmail.com>
2022-10-24 16:48:24 -0700

    Corrected Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,16 +8,25 @@
  */
 int main(void)
 {
-	int sum;
-	char c;
+	int i, sum, n;
+	int pass[100];
+
+	sum = 0;
 
 	srand(time(NULL));
-	while (sum <= 2645)
+
+	for (i = 0; i < 100; i++)
 	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-	putchar(2772 - sum);
 	return (0);
 }
