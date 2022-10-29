9da6ab88cce4278bb447a94ca63331f3ceb6614f
“Yusuf-kehinde” <yusufkehinde1454@gmail.com>
2022-10-28 17:15:43 -0700

    Tasks corrected

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,7 +11,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
  * @r_index: The current index of the buffer.
  *
  * Return: If r can store the sum - a pointer to the result.
- * 	If r cannot store the sum - 0.
+ * If r cannot store the sum - 0.
  */
 
 char *add_strings(char *n1, char *n2, char *r, int r_index)
@@ -59,7 +59,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
  * @size_r: The buffer size.
  *
  * Return: If r can store the sum - a pointer to the result.
- * 	If r cannot store the sum - 0.
+ * If r cannot store the sum - 0.
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,20 @@ char *cap_string(char *str)
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
-		if (str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' || str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' || str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' || str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == '{' || str[index - 1] == '}' || index == 0)
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' ||
+				index == 0)
 			str[index] -= 32;
 		index++;
 	}
